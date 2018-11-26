e5129f308fdf591b91d25a24a654c45cec27e9a6
UbuntuRus <alaxyu007@gmail.com>
2018-11-26 18:59:06 +0300

    my first commit

---
+++ games/urls_games.py
@@ -0,0 +1,15 @@
+from django.urls import re_path
+from . import views
+
+
+urlpatterns = [
+    re_path('all/', views.show),
+    re_path('showOne/(?P<video_id>\d+)/$', views.ShowOneVideo),
+    re_path('addcomment/(?P<video_id>\d+)/$', views.addcomment),
+    re_path('sign/',views.sign),
+    re_path('in/', views.inn),
+    re_path('out/', views.out),
+    re_path('addliketovideo/(?P<video_id>\d+)/$', views.addliketovideo),
+    re_path('addliketocomment/(?P<comment_id>\d+)/$', views.addliketocomment),
+    re_path('addliketovideo/ajax/', views.ajax),
+]
